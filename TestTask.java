9f88c9d52cef9ebe6530b656e4c92d994e2f8fbc
ATOM3448 <Qweru2003@gmail.com>
2024-08-26 00:12:03 +0700

    Stats added

--- TestTask.java
+++ TestTask.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
 class TestTask
 {
@@ -40,6 +41,22 @@ class TestTask
         // Объединим путь и префикс
         outPath += prefix;
 
+        // Переменные постоянной статистики
+        BigInteger counterInt = BigInteger.ZERO;
+        BigInteger counterFloat = BigInteger.ZERO;
+        BigInteger counterStr = BigInteger.ZERO;
+
+
+        // Переменные полной статистики
+        BigInteger minInt = BigInteger.ZERO;
+        BigInteger maxInt = BigInteger.ZERO;
+        BigInteger sumInt = BigInteger.ZERO;
+        BigDecimal minFloat = BigDecimal.ZERO;
+        BigDecimal maxFloat = BigDecimal.ZERO;
+        BigDecimal sumFloat = BigDecimal.ZERO;
+        int minStr = 0;
+        int maxStr = 0;
+
         try
         {
             // Потоки вывода результатов
@@ -56,22 +73,101 @@ class TestTask
                 try (BufferedReader fileIn = new BufferedReader(new FileReader(source)))
                 {
                     // Считываем строку и через регулярки проверяем тип
-                    while ((strBuf=fileIn.readLine())!=null)
+                    while ((strBuf=fileIn.readLine()) != null)
                     {
                         if (strBuf.matches("([-+]?\\d+)|(\\([-+]?\\d+\\))"))
                         {
                             integersOut.write(strBuf);
                             integersOut.newLine();
+
+                            counterInt = counterInt.add(BigInteger.ONE);
+
+                            if (!fullStat)
+                                continue;
+
+                            BigInteger bufInt = new BigInteger(strBuf.replace("(", "")
+                                                                     .replace(")", ""));
+
+                            if (counterInt.equals(BigInteger.ONE))
+                            {
+                                minInt = bufInt;
+                                maxInt = bufInt;
+                                sumInt = bufInt;
+                                continue;
+                            }
+
+                            sumInt = sumInt.add(bufInt);
+
+                            if (maxInt.compareTo(bufInt) == -1)
+                            {
+                                maxInt = bufInt;
+                            }
+                            else if (minInt.compareTo(bufInt) == 1)
+                            {
+                                minInt = bufInt;
+                            }
                         }
                         else if (strBuf.matches("[-+]?\\d+[.,]\\d+([EeЕе]\\^?(([-+]?\\d+)|(\\([-+]?\\d+\\))))?"))
                         {
                             floatsOut.write(strBuf);
                             floatsOut.newLine();
+
+                            counterFloat = counterFloat.add(BigInteger.ONE);
+
+                            if (!fullStat)
+                                continue;
+
+                            BigDecimal bufFloat = new BigDecimal(strBuf.replace("^", "")
+                                                                       .replace("(", "")
+                                                                       .replace(")", "")
+                                                                       .replace(",", "."));
+
+                            if (counterFloat.equals(BigInteger.ONE))
+                            {
+                                minFloat = bufFloat;
+                                maxFloat = bufFloat;
+                                sumFloat = bufFloat;
+                                continue;
+                            }
+
+                            sumFloat = sumFloat.add(bufFloat);
+
+                            if (maxFloat.compareTo(bufFloat) == -1)
+                            {
+                                maxFloat = bufFloat;
+                            }
+                            else if (minFloat.compareTo(bufFloat) == 1)
+                            {
+                                minFloat = bufFloat;
+                            }
                         }
                         else 
                         {
                             stringsOut.write(strBuf);
                             stringsOut.newLine();
+
+                            counterStr = counterStr.add(BigInteger.ONE);
+
+                            if (!fullStat)
+                                continue;
+
+                            int strLen = strBuf.length();
+
+                            if (counterStr.equals(BigInteger.ONE))
+                            {
+                                minStr = strLen;
+                                maxStr = strLen;
+                                continue;
+                            }
+
+                            if (strLen > maxStr)
+                            {
+                                maxStr = strLen;
+                            }
+                            else if (strLen < minStr)
+                            {
+                                minStr = strLen;
+                            }
                         }
                     }
                 }
@@ -95,5 +191,24 @@ class TestTask
         {
             System.out.printf("Ошибка работы с потоком записи\n%s\n", ex.getMessage());
         }
+
+        System.out.printf("Integer stats:\n\tCount: %s\n", counterInt);
+        if (fullStat && (counterInt.compareTo(BigInteger.ZERO) == 1))
+            System.out.printf("\tMax: %s\n\tMin: %s\n\tSum: %s\n\tAverange: %s\n", maxInt,
+                                                                                          minInt,
+                                                                                          sumInt,
+                                                                                          sumInt.divide(counterInt));
+        System.out.printf("Float stats:\n\tCount: %s\n", counterFloat);
+        if (fullStat && (counterFloat.compareTo(BigInteger.ZERO) == 1))
+            System.out.printf("\tMax: %s\n\tMin: %s\n\tSum: %s\n\tAverange: %s\n", maxFloat,
+                                                                                          minFloat,
+                                                                                          sumFloat,
+                                                                                          sumFloat.divide(new BigDecimal(counterFloat),
+                                                                                                    6,
+                                                                                                          RoundingMode.HALF_DOWN));
+        System.out.printf("String stats:\n\tCount: %s\n", counterStr);
+        if (fullStat && (counterStr.compareTo(BigInteger.ZERO) == 1))
+            System.out.printf("\tMax len: %s\n\tMin len: %s\n", maxStr,
+                                                                       minStr);
     }
 }
